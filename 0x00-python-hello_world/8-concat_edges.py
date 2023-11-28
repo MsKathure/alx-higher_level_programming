54c737d348c4ddce5d11b3c6ff48f610736ba316
MsKathure <kmmbaabu@gmail.com>
2023-11-28 03:12:28 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-original_str = "Python is an interpreted, interactive, object-oriented programming " \
-               "language that combines remarkable power with very clear syntax"
-modified_str = original_str[39:67] + original_str[115:120] + original_str[0:6]
-print(modified_str)
+original_str = "Python is an interpreted, interactive, object-oriented programming\
+		language that combines remarkable power with very clear syntax"
+str = original_str[39:67] + original_str[115:120] + original_str[0:6]
+print(str)
 
